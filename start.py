4775385bbfb09bb534d3627e277b361cb6523afd
Danglewood <85772166+deeleeramone@users.noreply.github.com>
2026-04-20 11:01:21 -0700

    add healthcheck endpoint and use app.json instead of CHECKS

--- start.py
+++ start.py
@@ -53,5 +53,10 @@ class RequireOpenBBUserMiddleware(BaseHTTPMiddleware):
 app.add_middleware(RequireOpenBBUserMiddleware)
 
 
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
 if __name__ == "__main__":
     launch_api(host="0.0.0.0", port=7750)
